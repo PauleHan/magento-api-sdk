054fca6d221c2fcdff270c4d2554cf1f6d384c59
Bladejakal <blade666@inbox.ru>
2016-04-27 17:22:59 +0300

    Update CommerceClient.php

--- src/Commerce/CommerceClient.php
+++ src/Commerce/CommerceClient.php
@@ -8,5 +8,15 @@ use Triggmine\TriggmineClient;
  *
  * @method \Triggmine\Result onAddProductInCart(array $args = [])
  * @method \GuzzleHttp\Promise\Promise onAddProductInCartAsync(array $args = [])
+ * @method \Triggmine\Result onFullCartChange(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise onFullCartChangeAsync(array $args = [])
+ * @method \Triggmine\Result onConvertCartToOrder(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise onConvertCartToOrderAsync(array $args = [])
+ * @method \Triggmine\Result onLogin(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise onLoginAsync(array $args = [])
+ * @method \Triggmine\Result onLogout(array $args = [])
+ * @method GuzzleHttp\Promise\Promise onLogoutAsync(array $args = [])
+ * @method \Triggmine\Result onCustomerRegister(array $args = [])
+ * @method GuzzleHttp\Promise\Promise onCustomerRegisterAsync(array $args = [])
  */
 class CommerceClient extends TriggmineClient{}
